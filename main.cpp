b2bfa0e4402d25464b5e6d3e4a004e98a346d4ac
MuxtorovicH <rusdosmux@gmail.com>
2024-05-11 23:18:42 +0500

    Homework 6

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//int num;
+
+//string findMaxOccurrence(const string &s1, const string &s2, char c, int &maxCount) {
+//    int count1 = count(s1.begin(), s1.end(), c);
+//    int count2 = count(s2.begin(), s2.end(), c);
+//
+//    maxCount = max(count1, count2);
+//    if (maxCount == 0) {
+//        return "Not found";
+//    }
+//    if (count1 == count2) {
+//        return "Same times";
+//    }
+//    return count1 > count2 ? s1 : s2;
+//}
+
+int main() {
+    //Problem 1
+//    int num;
+//    cout << "Enter a value for the global variable: ";
+//    cin >> ::num;
+//    cout << "Enter a value for the local variable: ";
+//    cin >> num;
+//    if (num == 0) {
+//        cout << "Error: Division by zero is not allowed." << endl;
+//        return 1;
+//    }
+//    int remainder = ::num % num;
+//    cout << "Output: " << remainder << endl;
+
+//Problem 2
+
+//    string s1, s2;
+//    char c;
+//
+//    cout << "Enter first string: ";
+//    cin >> s1;
+//    cout << "Enter second string: ";
+//    cin >> s2;
+//    cout << "Enter character to search: ";
+//    cin >> c;
+//
+//    int maxCount = 0;
+//    string result = findMaxOccurrence(s1, s2, c, maxCount);
+//
+//    if (result == "Not found") {
+//        cout << "Output: " << result << endl;
+//    } else if (result == "Same times") {
+//        cout << "Output: The character '" << c << "' appears " << result << " (" << maxCount << " times)." << endl;
+//    } else {
+//        cout << "Output: The string \"" << result << "\" contains the character '" << c << "' most times (" << maxCount
+//             << " times)." << endl;
+//    }
+
+
+    return 0;
+}
